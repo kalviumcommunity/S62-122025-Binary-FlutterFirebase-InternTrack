b266c55f7ce61ba31f809ab68382c851f92ff1bd
varshapanda <varsha.vijayashwini@kalvium.community>
2026-01-07 10:41:01 +0530

    feat: integrated Firebase SDKs using FlutterFire CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:s62_122025_binary_flutterfirebase_interntrack/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   runApp(const InternTrackApp());
 }
 
